8df3a91ff4a168dae5a233a71bf2b60253ac352c
LiantsoaRasolofoson <liantsoarasolofoson@gmail.com>
2023-01-18 00:10:41 +0300

    update v_enchereRepository

--- etu1650-etu1616-etu1534-etu1522/src/main/java/com/example/enchere/repository/V_EnchereRepository.java
+++ etu1650-etu1616-etu1534-etu1522/src/main/java/com/example/enchere/repository/V_EnchereRepository.java
@@ -1,9 +1,6 @@
 package com.example.enchere.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import com.example.enchere.modele.Offre;
 import com.example.enchere.modele.V_Enchere;
 
 public interface V_EnchereRepository extends JpaRepository<V_Enchere, Integer> {
