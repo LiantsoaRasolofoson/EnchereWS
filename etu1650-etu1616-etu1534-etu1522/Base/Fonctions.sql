2ed593b88b21eea0c383239d115cc0601a7ac600
LiantsoaRasolofoson <liantsoarasolofoson@gmail.com>
2023-01-20 06:36:48 +0300

    update fonction.sql

--- etu1650-etu1616-etu1534-etu1522/Base/Fonctions.sql
+++ etu1650-etu1616-etu1534-etu1522/Base/Fonctions.sql
@@ -1,3 +1,31 @@
+-- CREATE OR REPLACE FUNCTION status(id integer) RETURNS VARCHAR AS $$
+--     DECLARE
+--         dates timestamp;
+--         fin timestamp;
+--         temoin integer;
+--     BEGIN
+--         SELECT dateEnchere,dateEnchere+duree INTO dates,fin FROM Enchere WHERE idEnchere = id;
+--         SELECT count(*) INTO temoin FROM EnchereVendu WHERE idEnchere = id;
+--         IF now() > dates 
+--         THEN
+--             IF now() < fin
+--             THEN
+--                 RETURN 'En Cours';
+--             ELSE
+--                 IF temoin = 0
+--                 THEN
+--                     RETURN 'Non-Vendu';
+--                 ELSE
+--                     RETURN 'Vendu'; 
+--                 END IF; 
+--             END IF; 
+--         ELSE
+--             RETURN 'Non demarrer';
+--         END IF;
+--     END;
+-- $$ LANGUAGE plpgsql;
+
+
 CREATE OR REPLACE FUNCTION status(id integer) RETURNS VARCHAR AS $$
     DECLARE
         dates timestamp;
@@ -12,12 +40,7 @@ CREATE OR REPLACE FUNCTION status(id integer) RETURNS VARCHAR AS $$
             THEN
                 RETURN 'En Cours';
             ELSE
-                IF temoin = 0
-                THEN
-                    RETURN 'Non-Vendu';
-                ELSE
-                    RETURN 'Vendu'; 
-                END IF; 
+                RETURN 'Termine';
             END IF; 
         ELSE
             RETURN 'Non demarrer';
